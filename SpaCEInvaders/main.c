5b2fe28550541f08f623a6a7a2501e5ccea62b47
SebasteuoAlba <sebasjosue84@gmail.com>
2019-04-06 01:51:37 -0600

    Initial Commit

---
+++ SpaCEInvaders/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  SpaCEInvaders
+//
+//  Created by Sebastian Alba on 4/6/19.
+//  Copyright © 2019 Sebastian Alba. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
